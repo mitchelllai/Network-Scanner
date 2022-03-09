ac5657c89a420a72f539942362d7748a5f23a971
Mitchell Min-Jun Lai <mml7841@moore.wot.eecs.northwestern.edu>
2022-03-09 12:29:31 -0600

    write to json; framework done

--- scan.py
+++ scan.py
@@ -1,13 +1,13 @@
 from sys import argv
 from json import dump
+from time import time
 
 json_object = {}
 with open(argv[1], 'r') as input:
     websites = input.read().splitlines()
     for website in websites:
-        json_object[website] = {}
+        json_object[website] = {'scan_time': time()}
 
-# with open(argv[2], 'w') as output:
-#     dump(json_object, f, sort_keys=True, indent=4)
+with open(argv[2], 'w') as output:
+    dump(json_object, output, sort_keys=True, indent=4)
 
-print(json_object)
